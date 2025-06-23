c7337f9b3b866a43d9fa34f22912d604fd4a348b
Nath9666 <nathan.morel@efrei.net>
2025-06-24 00:49:35 +0200

    debut de la securité des parametre

--- main.py
+++ main.py
@@ -2,18 +2,42 @@ import pandas as pd
 import sys
 import configparser
 import xlwings as xw
+import os
+import colorama
+from colorama import Fore, Style
 
 
 def lire_config(chemin_config):
     config = configparser.ConfigParser()
     config.read(chemin_config, encoding='utf-8')
+    
+    #? parametre concernant le graphique
+    if 'Graphique' not in config:
+        raise ValueError("La section 'Graphique' est manquante dans le fichier de configuration.")
     params = config['Graphique']
-    largeur = int(params.get('largeur', 800))
-    hauteur = int(params.get('hauteur', 600))
     arrondi = int(params.get('arrondi', 1))
-    couleur_ligne = params.get('couleur_ligne', 'blue')
-    return largeur, hauteur, arrondi, couleur_ligne
 
+    #? Couleur
+    couleur_ligne = params.get('couleur_ligne', '#0000FF')
+    couleur_ligne = HexToRGB(couleur_ligne)
+    couleur_ligne_secondaire = params.get('couleur_ligne_secondaire', '#FF0000')
+    couleur_ligne_secondaire = HexToRGB(couleur_ligne_secondaire)
+    couleur_fond = params.get('couleur_fond', '#FFFFFF')
+    couleur_fond = HexToRGB(couleur_fond)
+    couleur_axes = params.get('couleur_axes', '#CCCCCC')
+    couleur_axes = HexToRGB(couleur_axes)
+
+    largeur_ligne = int(params.get('largeur_ligne', 2))
+    hauteur_ligne = int(params.get('hauteur_ligne', 2))
+    legende = params.get('legende', 'oui').lower() == 'oui'
+    quadrillage = params.get('quadrillage', 'oui').lower() == 'oui'
+    marqueur = params.get('marqueur', 'non').lower() == 'oui'
+
+    return arrondi, couleur_ligne, couleur_ligne_secondaire, couleur_fond, couleur_axes, largeur_ligne, hauteur_ligne, legende, quadrillage, marqueur
+
+def HexToRGB(hex_color):
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i + 2], 16) for i in (0, 2, 4))
 
 def lire_donnees_excel(chemin_fichier):
     try:
@@ -74,9 +98,41 @@ def read_parameters():
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage : python main.py <chemin_fichier_excel>")
+    errors = []
+    messageHelp = "Faite --help si vous avez besoin d'aide."
+    colorama.init(autoreset=True)
+    if len(sys.argv) < 4:
+        errors.append(Fore.RED + Style.BRIGHT + "Erreur : Nombre de paramètres insuffisant 3 demandé")
+        errors.append(Fore.YELLOW + Style.BRIGHT + "Veuillez fournir le chemin du fichier Excel, le nom de la colonne de référence et la liste des données.")
+        errors.append(Fore.CYAN + Style.BRIGHT + "Exemple : python main.py ./data/Classeur1.xlsx Date [[Données 1, 10, 15, Titre 1, #FF0000], [Données 2, 4, 15, Titre 2, #00FF00]]")
+        errors.append(Fore.GREEN + Style.BRIGHT + messageHelp)
+        exit(1)
+    
+    if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+        print(Fore.GREEN + Style.BRIGHT + "Aide :")
+        print("Usage : python main.py <chemin_fichier_excel> <Nom de la colonne de référence> [[<Nom de la donnée>, <Min>, <Max>, ?Titre, ?Couleur], ...]\n"+"Exemple : python main.py ./data/Classeur1.xlsx Date [[Données 1, 10, 15, Titre 1, #FF0000], [Données 2, 4, 15, Titre 2, #00FF00]]")
+        sys.exit(0)
+    
+    if sys.argv[1] is None or sys.argv[1] == "" or not sys.argv[1].endswith('.xlsx') or not os.path.exists(sys.argv[1]):
+        errors.append(Fore.RED + Style.BRIGHT + "Le chemin du fichier Excel est requis.")
+
+    if sys.argv[2] is None or sys.argv[2] == "" or not isinstance(sys.argv[2], str) or sys.argv[2].isdigit():
+        errors.append(Fore.RED + Style.BRIGHT + "Le nom de la colonne de référence doit être une chaîne de caractères non vide soit un string")
+
+    if sys.argv[3] is None or sys.argv[3] == "" or not isinstance(sys.argv[3],list):
+        errors.append(Fore.RED + Style.BRIGHT + "La liste des données doit être une liste non vide.")
+
+    if len(sys.argv[3]) < 1 and not all(isinstance(item, list) and len(item) == 3 for item in sys.argv[3]):
+        errors.append(Fore.RED + Style.BRIGHT + "Chaque élément de la liste des données doit être une liste de 3 éléments minimum : [Nom, Min, Max]. Si un titre ou une couleur est fourni, il doit être ajouté à la fin de la liste.")
+
+    if len(errors) > 0:
+        for error in errors:
+            print(error)
+        print(Fore.CYAN + "Veuillez corriger les erreurs et réessayer.\n" + messageHelp)
+        sys.exit(1)
     else:
+        print("Paramètres valides, traitement en cours...")   
+
         chemin_excel = sys.argv[1]
         chemin_config = "config.ini"
         out_xlsx = "out.xlsx"
